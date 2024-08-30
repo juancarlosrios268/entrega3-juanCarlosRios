53cad72a6a2166fa439c575ece68f4d74da81394
juan carlos rios <juancarlosriosr@gmail.com>
2024-08-30 14:52:54 -0500

    commit entrega final

--- script.js
+++ script.js
@@ -1,88 +1,105 @@
 
+fetch("./info.json")
+    .then(response => response.json())
+    .then(productos =>principal(productos))
+    .catch(error => console.log(error))
 
-let productos = [
-    {id: 1209, nombre:"play 5" , precio:2000000 , cantidad:7 , categoria:"consola", rutaImagen:"play5.png" },
-    {id: 1529, nombre:"xbox" , precio:1400000 , cantidad:12 , categoria:"consola", rutaImagen:"xbox.png" },
-    {id: 2234, nombre:"nintento" , precio:1000000 , cantidad:5 , categoria:"consola", rutaImagen:"nintendo.png" },
-    {id: 5599, nombre:"mario bross" , precio:250000 , cantidad:11 , categoria:"video juego", rutaImagen:"mario.jpg" },
-    {id: 4677, nombre:"fornite" , precio:100000 , cantidad:34 , categoria:"video juego", rutaImagen:"fortnite.png"},
-    {id: 7689, nombre:"fifa" , precio:280000 , cantidad:75 , categoria:"video juego", rutaImagen:"fifa.jpeg" },
-    {id: 1123, nombre:"audifono" , precio:25000 , cantidad:20 , categoria:"accesorio", rutaImagen:"diadema.jpeg" },
-    {id: 1198, nombre:"microfono" , precio:80000 , cantidad:15 , categoria:"accesorio", rutaImagen: "microfono.jpeg"},
-    {id: 4430, nombre:"pc gamer" , precio:4000000 , cantidad:2 , categoria:"consola", rutaImagen: "pcGamer.png"},
-];
 
-let contenedorProductos = document.getElementById("productos");
+function principal(productos){
 
-productos.forEach(producto => {
-    let divProducto = document.createElement('div');
-    divProducto.className = 'producto';
+    let contenedorProductos = document.getElementById("productos");
 
-    divProducto.innerHTML = `
-        <h3>${producto.nombre}</h3>
-        <p>$ ${producto.precio}</p>
-        <p>Cantidad de productos: ${producto.cantidad}</p>
-        <img src="./images/${producto.rutaImagen}" alt="${producto.nombre}">
-        <button class=botonAñadir id="${producto.id}">Añadir al Carrito</button>
-    `;
+    productos.forEach(producto => {
+        let divProducto = document.createElement('div');
+        divProducto.className = 'producto';
 
-    contenedorProductos.appendChild(divProducto);
-});
+        divProducto.innerHTML = `
+            <h3>${producto.nombre}</h3>
+            <p>$ ${producto.precio}</p>
+            <p>Cantidad de productos: ${producto.cantidad}</p>
+            <img src="./images/${producto.rutaImagen}" alt="${producto.nombre}">
+            <button class=botonAñadir id="${producto.id}">Añadir al Carrito</button>
+        `;
 
-let carrito = []
-let carritoTotal = 0
+        contenedorProductos.appendChild(divProducto);
+    });
 
-productos.forEach(producto => {
-    let botonAgregarAlCarrito = document.getElementById(producto.id);
-        botonAgregarAlCarrito.addEventListener("click", function(e) {
-            let productId = e.target.id
-            let productoAgredado = productos.find((producto) => producto.id == productId)
-            carrito.push({
-                nombre: productoAgredado.nombre,
-                precio:productoAgredado.precio,
-                id: productoAgredado.id
-            })
-            carritoTotal+=productoAgredado.precio
-            carritoTotalDiv.innerText= carritoTotal
-
-            renderCarrito()
-        });
-})
-
-let lista = document.querySelector("#dynamicList");
-let carritoTotalDiv = document.querySelector("#carritoTotal");
-
-function renderCarrito() {
-    lista.innerHTML=""
-    carrito.forEach(item => {
-        console.log(item)
-        let newItem= document.createElement("li");
-        newItem.innerText= item.nombre + item.precio;
-        lista.appendChild(newItem);
-    })
-    setearCarrito()
-}
-
-function setearCarrito() {
-    let carritoJSON = JSON.stringify(carrito)
-    localStorage.setItem("carrito", carritoJSON)
-}
-
-obtenerCarrito()
-function obtenerCarrito() {
-    console.log("asd")
-
-    if (localStorage.getItem("carrito")) {
-        carrito = JSON.parse(localStorage.getItem("carrito"))
+    function lanzarAlerta (){
+        Toastify({
+            text: "Se agregó el producto al carrito",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+            background: "linear-gradient(to right, #deb887, #96c93d)",
+            },
+            onClick: function(){lanzarAlerta("prueba")} // Callback after click
+        }).showToast();
     }
-    
-    renderCarrito()
-}
 
-let buttonDelete = document.getElementById('deleteButton')
-buttonDelete.addEventListener('click', function() {
-    console.log('click delete')
-    localStorage.removeItem("carrito")
-    carrito = []
-    renderCarrito()
-})
+    let carrito = []
+    let carritoTotal = 0
+
+    productos.forEach(producto => {
+        let botonAgregarAlCarrito = document.getElementById(producto.id);
+            botonAgregarAlCarrito.addEventListener("click", function(e) {
+                let productId = e.target.id
+                let productoAgredado = productos.find((producto) => producto.id == productId)
+                carrito.push({
+                    nombre: productoAgredado.nombre,
+                    precio:productoAgredado.precio,
+                    id: productoAgredado.id
+                })
+                carritoTotal+=productoAgredado.precio
+                carritoTotalDiv.innerText= carritoTotal
+
+                renderCarrito()
+                lanzarAlerta ()
+            });
+    })
+
+
+
+    let lista = document.querySelector("#dynamicList");
+    let carritoTotalDiv = document.querySelector("#carritoTotal");
+
+    function renderCarrito() {
+        lista.innerHTML=""
+        carrito.forEach(item => {
+            let newItem= document.createElement("li");
+            newItem.innerText= item.nombre + item.precio;
+            lista.appendChild(newItem);
+        })
+        setearCarrito()
+    }
+
+    function setearCarrito() {
+        let carritoJSON = JSON.stringify(carrito)
+        localStorage.setItem("carrito", carritoJSON)
+    }
+
+    obtenerCarrito()
+    function obtenerCarrito() {
+
+        if (localStorage.getItem("carrito")) {
+            carrito = JSON.parse(localStorage.getItem("carrito"))
+        }
+        
+        renderCarrito()
+    }
+
+    let buttonDelete = document.getElementById('deleteButton')
+    buttonDelete.addEventListener('click', function() {
+        localStorage.removeItem("carrito")
+        carrito = []
+        carritoTotalDiv.innerText=0
+        renderCarrito()
+    })
+
+    }
+
+
+    
